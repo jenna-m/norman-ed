0cca937ac910531f047e82f93133317161f9bf6f
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-25 18:43:32 -0400

    Update footer.js

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 class Footer extends React.PureComponent {
     render() {
         return (
             <div className="footer">
-                <p>© Norman Ed 1975-2019</p>
-                <p>Website design by <a href="https://github.com/jenna-m">jenna-m</a>.</p>
+                <p>Artwork © Norman Ed 1975-2019</p>
+                <p>Website design by <a href="https://github.com/jenna-m">jenna-m</a></p>
             </div>
 
         );
