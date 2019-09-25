694ef6bbd72499e7d9905fd3addb7755525535d6
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-25 18:35:35 -0400

    Create preCollegiate.js
    Create Gallery subpages

---
+++ client/src/components/gallery/preCollegiate.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PreCollegiate extends React.Component {
+    render() {
+        return (
+            <div className="gallery-container">
+                Pre-Collegiate content
+            </div>
+        );
+    }
+}
+
+export default PreCollegiate;
