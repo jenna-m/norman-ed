1aa033bb4456e8326c9070c8dd1e84c2c1c84d06
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-25 18:35:32 -0400

    Create undergrad.js
    Create Gallery subpages

---
+++ client/src/components/gallery/undergrad.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Undergrad extends React.Component {
+    render() {
+        return (
+            <div className="gallery-container">
+                Undergrad content
+            </div>
+        );
+    }
+}
+
+export default Undergrad;
