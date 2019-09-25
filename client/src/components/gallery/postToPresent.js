77c69091b59f654c1b6f8734171e1a126f534b7e
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-25 18:35:39 -0400

    Create postToPresent.js
    Create Gallery subpages

---
+++ client/src/components/gallery/postToPresent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PostToPresent extends React.Component {
+    render() {
+        return (
+            <div className="gallery-container">
+                Post-Collegiate to Present content
+            </div>
+        );
+    }
+}
+
+export default PostToPresent;
