8280639a664cd7796e509e3e92c512d2e8ac9789
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-25 18:34:59 -0400

    Update navbar.js
    Add dropdown menus

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -5,16 +5,30 @@ class Navbar extends React.Component {
     render() {
         return (
             <div className="navbar" role="navigation" aria-label="Main">
-                <div className="menu">
-                    <ul>
-                        <li><NavLink to="/bio">About</NavLink></li>
-                        <li><NavLink to="/artist-statement">Artist Statement</NavLink></li>
-                        <li><NavLink to="/gallery">Gallery</NavLink></li>
-                        <li><NavLink to="/exhibits">Exhibits</NavLink></li>
-                        <li><NavLink to="/press">Press</NavLink></li>
-                        <li><NavLink to="/contact">Contact</NavLink></li>
-                    </ul>
+                <div className="logo">
+                    Norman Ed
                 </div>
+                <ul className="menu">
+                    <li>
+                        <li className="menu-category"><NavLink to="/">About</NavLink></li>
+                        <ul className="dropdown">
+                            <li className="sublink"><NavLink to="/bio">Biography</NavLink></li>
+                            <li className="sublink"><NavLink to="/artist-statement">Artist Statement</NavLink></li>    
+                        </ul>
+                    </li>
+                    <li>
+                        <li className="menu-category"><NavLink to="/">Gallery</NavLink></li>
+                        <ul className="dropdown">
+                            <li className="sublink"><NavLink to="/gallery">All Work</NavLink></li>
+                            <li className="sublink"><NavLink to="/pre-collegiate">Pre-Collegiate</NavLink></li>
+                            <li className="sublink"><NavLink to="/undergrad">Undergrad</NavLink></li>
+                            <li className="sublink"><NavLink to="/post-present">1982 to Present</NavLink></li>    
+                        </ul>
+                    </li>
+                    <li className="menu-category"><NavLink to="/exhibits">Exhibits</NavLink></li>
+                    <li className="menu-category"><NavLink to="/press">Press</NavLink></li>
+                    <li className="menu-category"><NavLink to="/contact">Contact</NavLink></li>
+                </ul>
             </div>
         );
     }
