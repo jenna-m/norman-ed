52e8d5355e747b553ee612a39870a29928a659e8
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-26 10:58:33 -0400

    Update undergrad.js
    Add logic to filter items by gallery category

--- client/src/components/gallery/undergrad.js
+++ client/src/components/gallery/undergrad.js
@@ -1,14 +1,35 @@
 // Renders all works with the gallery 'undergrad'
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { galleryImages } from '../../constants/gallery/galleryDatabase';
 
 class Undergrad extends React.Component {
     render() {
+        let undergradWork = galleryImages.map(item => {
+            if (item.gallery === 'undergrad') {
+                return (
+                    <div className="item-card" key={item.id}>
+                        <div className="item-image">
+                            <Link to = {{ 
+                                pathname:`/gallery/${item.gallery}`,
+                                search: `?id=${item.id}`
+                            }}>
+                                <img src={item.src[0]} alt={item.title} />
+                            </Link>
+                        </div>
+                    </div>
+                )
+            }
+        })
         return (
             <div className="container">
                 <h1>Undergrad</h1>
+                <div className="gallery-container">
+                    { undergradWork }
+                </div>
             </div>
         );
     }
 }
 
-export default Undergrad;
+export default withRouter(Undergrad);
