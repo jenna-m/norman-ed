ec7f4123846238b083549bcd2e30ba8598237e27
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-26 10:58:27 -0400

    Update preCollegiate.js
    Add logic to filter items by gallery category

--- client/src/components/gallery/preCollegiate.js
+++ client/src/components/gallery/preCollegiate.js
@@ -1,14 +1,36 @@
 // Renders all works with the gallery 'pre'
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { galleryImages } from '../../constants/gallery/galleryDatabase';
+
 
 class PreCollegiate extends React.Component {
     render() {
+        let preCollegiateWork = galleryImages.map(item => {
+            if (item.gallery === 'pre') {
+                return (
+                    <div className="item-card" key={item.id}>
+                        <div className="item-image">
+                            <Link to = {{ 
+                                pathname:`/gallery/${item.gallery}`,
+                                search: `?id=${item.id}`
+                            }}>
+                                <img src={item.src[0]} alt={item.title} />
+                            </Link>
+                        </div>
+                    </div>
+                )
+            }
+        })
         return (
             <div className="container">
                 <h1>Pre-Collegiate</h1>
+                <div className="gallery-container">
+                    { preCollegiateWork }
+                </div>
             </div>
         );
     }
 }
 
-export default PreCollegiate;
+export default withRouter(PreCollegiate);
