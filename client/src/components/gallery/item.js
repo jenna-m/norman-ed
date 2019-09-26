3e9eff05d7149baaa589a513c78d14f0c5824430
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-26 14:47:22 -0400

    Update item.js
    Build Item page

--- client/src/components/gallery/item.js
+++ client/src/components/gallery/item.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { galleryImages } from '../../constants/gallery/galleryDatabase';
+
 
 class Item extends React.Component {
+    constructor(props) {
+        super(props);
+
+        let currentItemId = new URLSearchParams(window.location.search).get("id");
+        
+        if (currentItemId) {
+          currentItemId = parseInt(currentItemId, 10);
+        }
+
+        this.state = {
+          item: galleryImages.find(item => item.id === currentItemId)
+        };
+
+      }
+
     render() {
+        let item = this.state.item;
         return (
             <div className="container">
-                Individual Item content
+                <h1>{item.title}</h1>
+                <div className="item-details" key={item.id}>
+                    <div className="item-details-img">
+                        <img src={item.src[0]} alt={item.title} />
+                    </div>
+                    <div className="item-details-info">
+                        <p>{item.date}</p>
+                        <p>{item.media}</p>
+                        <p>{item.dimensions}</p>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default Item;
+export default withRouter(Item);
