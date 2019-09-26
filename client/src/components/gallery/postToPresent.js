ba4744cbe8b32be6ae30f2615351e1565c9ca169
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-26 10:58:22 -0400

    Update postToPresent.js
    Add logic to filter items by gallery category

--- client/src/components/gallery/postToPresent.js
+++ client/src/components/gallery/postToPresent.js
@@ -1,15 +1,36 @@
 // Renders all works with the gallery 'post'
 // Have sidebar with links for individual years
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import { galleryImages } from '../../constants/gallery/galleryDatabase';
 
 class PostToPresent extends React.Component {
     render() {
+        let postToPresentWork = galleryImages.map(item => {
+            if (item.gallery === 'post') {
+                return (
+                    <div className="item-card" key={item.id}>
+                        <div className="item-image">
+                            <Link to = {{ 
+                                pathname:`/gallery/${item.gallery}`,
+                                search: `?id=${item.id}`
+                            }}>
+                                <img src={item.src[0]} alt={item.title} />
+                            </Link>
+                        </div>
+                    </div>
+                )
+            }
+        })
         return (
             <div className="container">
                 <h1>1982 - Present</h1>
+                <div className="gallery-container">
+                    { postToPresentWork }
+                </div>
             </div>
         );
     }
 }
 
-export default PostToPresent;
+export default withRouter(PostToPresent);
