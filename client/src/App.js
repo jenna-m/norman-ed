e646c941f861aea82fce494a12ac33e1765b9d4b
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-26 14:47:03 -0400

    Update App.js
    Wrap Main in BrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Main from './components/main';
 import './App.css';
 
 
 function App() {
   return (
-    <Main />
+    <Router>
+      <Main />
+    </Router>
   );
 }
 
