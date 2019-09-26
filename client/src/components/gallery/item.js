4c465d690ea252abd4c0195626dd2eac7010fed3
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-25 22:22:06 -0400

    Create item.js

---
+++ client/src/components/gallery/item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Item extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                Individual Item content
+            </div>
+        );
+    }
+}
+
+export default Item;
