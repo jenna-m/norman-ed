c87e8dcf4a12c0216c78de9a44b9625eb1627bf2
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-27 13:12:32 -0400

    Rename from exhibits.js to exhibitions.js
    Change wording from 'exhibits' to 'exhibitions'

---
+++ client/src/components/pages/exhibitions.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { exhibitionsInfo } from '../../constants/exhibitions/exhibitionsDatabase';
+
+
+class Exhibitions extends React.Component {
+    render() {
+        let exhibitions = exhibitionsInfo.map(item => {
+            return (
+                <div className="exhibition-card" key={item.id}>
+                    <div className="exhibition-image">
+                        <img src={item.src} />
+                    </div>
+                    <div className="exhibition-info">
+                        <ul>
+                            <li className="year">{item.year}</li>
+                            <li className="title"><a href={item.url} target="new">{item.title}</a></li>
+                            <li className="venue-and-location">{item.venue} | {item.location}</li>
+                            <li className="description">{item.desc}</li>
+                            <li className="date">{item.date}</li>
+                        </ul>
+                    </div>
+                </div>
+            )
+        })
+        return (
+            <div>
+                <div className="container">
+                    <h1>Exhibitions</h1>
+                </div>
+                <div className="exhibit-list">
+                    { exhibitions }
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Exhibitions;
