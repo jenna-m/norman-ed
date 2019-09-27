3c01a07b7d91a80f800a8fdfb58773990e970557
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-27 15:14:44 -0400

    Create pressDatabase.js
    Build pressDatabase file (to be mapped on Press page)

---
+++ client/src/constants/press/pressDatabase.js
@@ -0,0 +1,47 @@
+/* This file contains press links that will be mapped out on the Press page.
+The 'year' key should be kept as an integer.
+*/
+
+
+export const pressArticles = [
+    {
+        title: 'Family exhibition at Westmont arts center is "true, unique and diverse"',
+        excerpt: '',
+        source: 'The Tribune-Democrat',
+        url: 'https://www.tribdem.com/features/family-exhibition-at-westmont-arts-center-is-true-unique-and/article_a4ce17e2-d0d1-11e9-953a-2bf74d99d89d.html',
+        date: 'September 7, 2019',
+        year: 2019
+    },
+    {
+        title: 'This new exhibit showcases the best art from across Pennsylvania',
+        excerpt: 'Artwork has been chosen for "Art of the State," the annual juried exhibition that has been showcasing the work of Pennsylvania artists since 1968. The mediums span painting, photography, craft, sculpture and work on paper. The exhibit includes 130 works of art by 119 artists from 28 counties. This year’s exhibit attracted nearly 2,200 entries from 845 artists.',
+        source: 'PennLive.com',
+        url: 'https://www.pennlive.com/life/2017/06/2017_art_of_the_state_exhibit.html',
+        date: 'June, 2017',
+        year: 2017
+    },
+    {
+        title: 'Sparks fly: Trish Crapo learns that, though loud and dirty, welding turns out to be a delicate task',
+        excerpt: 'During the school year, Ed is a sculptor and a high school art teacher in Johnstown, Pa., but he’s been coming to Snow Farm to teach during the summers for more than 20 years... He brings competence, patience and a sense of friendly good humor to our class of five women, who range in age from mid 40s to early 70s, and one male — a high school junior.',
+        source: 'Greenfield Recorder',
+        url: 'https://www.recorder.com/Loud-dirty-and-full-of-sparks-welding-a-delicate-task-4439810',
+        date: 'August 31, 2016',
+        year: 2016
+    },
+    {
+        title: 'Painter and sculptor come together at Johnstown art show',
+        excerpt: 'Norman Ed, the exhibit\'s sculptor and Johnstown native, said he will be working on the pieces up until the day of the show... "The majority of the work is all buttoned up," Ed said, "but I love the energy and the certain kind of tension and frustration about trying to pull something off."',
+        source: 'WESA Pittsburgh',
+        url: 'https://www.wesa.org/post/painter-and-sculptor-come-together-johnstown-art-show#stream/0',
+        date: 'May 28, 2012',
+        year: 2012
+    },
+    {
+        title: 'Many curious about downtown sculpture',
+        excerpt: '',
+        source: 'The Tribune-Democrat',
+        url: 'https://www.tribdem.com/news/local_news/many-curious-about-downtown-sculpture/article_0d493891-6335-5fdd-9f56-3e69be7bde07.html',
+        date: 'November 13, 2009',
+        year: 2009
+    },
+];
