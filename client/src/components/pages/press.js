f66d85cb2b892e253f07c77ef538f7dda2741814
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-27 15:14:23 -0400

    Update press.js
    Build Press page

--- client/src/components/pages/press.js
+++ client/src/components/pages/press.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { pressArticles } from '../../constants/press/pressDatabase';
 
 class Press extends React.Component {
     render() {
+        let articles = pressArticles.map(item => {
+            return (
+                <div>
+                    <li className="press-title"><a href={item.url} target="new">"{item.title}"</a></li>
+                    <li className="press-date">{item.date}</li>
+                    <li className="press-excerpt">{item.excerpt}</li>
+                    <li className="press-source">Source: {item.source}</li>
+                </div>
+            )
+        })
         return (
-            <div className="container">
-                <h1>Press</h1>
+            <div>
+                <div className="container">
+                    <h1>Press</h1>
+                </div>
+                <div className="press-list">
+                    <ul>
+                        { articles }
+                    </ul>
+                </div>
             </div>
         );
     }
