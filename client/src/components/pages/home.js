5ee136592c9ac19e57978c9354c407446650657e
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-27 10:13:16 -0400

    Update home.js
    Build Home page, add RandomImage component

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import RandomImage from '../randomImage'; // something's broken w/ this
 
 class Home extends React.Component {
     render() {
         return (
-            <div className="container">
-                Home content
+            <div className="home-container">
+                <div className="home-side-text">
+                    <h1>Artist</h1>
+                    <h1>Welder</h1>
+                    <h1>Sculptor</h1>
+                    <h1>Builder</h1>
+                    <h1>Teacher</h1>
+                </div>
+                <div className="home-random-image">
+                    <RandomImage />
+                </div>
             </div>
         );
     }
