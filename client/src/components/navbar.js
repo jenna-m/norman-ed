7298e414c06ed701a47fb884574eecf64e156004
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-28 09:23:16 -0400

    Remove AllWork component
    Delete allWork.js, and remove from navbar and routing

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -12,7 +12,6 @@ class Navbar extends React.Component {
                     <li>
                         <li className="menu-category"><NavLink to="/gallery">Gallery</NavLink></li>
                         <ul className="dropdown">
-                            <li className="sublink"><NavLink to="/all-work">All Work</NavLink></li>
                             <li className="sublink"><NavLink to="/pre-collegiate">Pre-Collegiate</NavLink></li>
                             <li className="sublink"><NavLink to="/undergrad">Undergrad</NavLink></li>
                             <li className="sublink"><NavLink to="/post-present">1982 to Present</NavLink></li>    
--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -6,7 +6,6 @@ import Home from '../components/pages/home';
 import Bio from '../components/about/bio';
 import ArtistStatement from '../components/about/artistStatement';
 import Gallery from '../components/gallery/gallery';
-import AllWork from '../components/gallery/allWork';
 import PreCollegiate from '../components/gallery/preCollegiate';
 import Undergrad from '../components/gallery/undergrad';
 import PostToPresent from '../components/gallery/postToPresent';
@@ -20,16 +19,15 @@ export const Routes =
     <div>
         <Switch>
             <Route exact path="/" component={ Home }/>
-            <Route path="/bio" component={ Bio }/>
-            <Route path="/artist-statement" component={ ArtistStatement }/>
             <Route path="/gallery" component={ Gallery }/>
-            <Route path="/all-work" component={ AllWork }/>
             <Route path="/pre-collegiate" component={ PreCollegiate }/>
             <Route path="/undergrad" component={ Undergrad }/>
             <Route path="/post-present" component={ PostToPresent }/>
             <Route path="/art" component={ Item }/>
             <Route path="/exhibitions" component={ Exhibitions }/>
             <Route path="/press" component={ Press }/>
+            <Route path="/bio" component={ Bio }/>
+            <Route path="/artist-statement" component={ ArtistStatement }/>
             <Route path="/contact" component={ Contact }/>
             <Route component={ NotFound }/>
         </Switch>
