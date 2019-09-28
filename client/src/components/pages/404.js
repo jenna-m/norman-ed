0430872f9fc108fb8ad58122bf72becb9b77ed80
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-27 20:08:12 -0400

    Update favicon

--- client/src/components/pages/404.js
+++ client/src/components/pages/404.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import RandomImage from '../randomImage';
 
 class NotFound extends React.Component {
     render() {
         return (
             <div className="container">
-                404 page
+                <h1>404 / Not Found</h1>
+                <div className="_404-container">
+                    <p>Sorry, that page can't be found.</p>
+                    <RandomImage />
+                </div>
             </div>
         );
     }
