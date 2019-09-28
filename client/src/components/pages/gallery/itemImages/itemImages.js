c0b1fd049a8e48de0ee5aaacca09de72eafe17b8
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-28 17:41:28 -0400

    Create itemImages.js
    Create component that iterates over item.src array and pushes <img>'s to an array to render all images for a selected gallery item

---
+++ client/src/components/pages/gallery/itemImages/itemImages.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { galleryImages } from '../../../../constants/gallery/galleryDatabase';
+import RenderImage from './renderImage';
+
+
+class ItemImages extends React.Component {
+    constructor(props) {
+        super(props);
+
+        let currentItemId = new URLSearchParams(window.location.search).get("id");
+        
+        if (currentItemId) {
+          currentItemId = parseInt(currentItemId, 10);
+        }
+
+        this.state = {
+          item: galleryImages.find(item => item.id === currentItemId)
+        };
+
+      }
+
+    render() {
+        const item = this.state.item;
+
+        // define array to be rendered by ItemImages component
+        const itemImages = [];
+
+        // forEach element in item.src...
+        item.src.forEach( (src, i) => {
+            /* pass props src and index to RenderImage component,
+            and push to itemImages array */
+            itemImages.push(<RenderImage imageUrl={src} key={i} />)
+        });
+        
+        return (
+          <div className="item-details-images">
+            {itemImages}
+          </div>
+        );
+    }
+}
+
+export default ItemImages;
