830ed0446c5afef4112568041a78630dbaef78fb
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-30 14:12:58 -0400

    Update item.js
    Add logic to go to previous and next item in gallery

--- client/src/components/pages/gallery/item.js
+++ client/src/components/pages/gallery/item.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import { galleryImages } from '../../../constants/gallery/galleryDatabase';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import ItemImages from './itemImages/itemImages';
 
 
@@ -11,29 +13,65 @@ class Item extends React.Component {
         let currentItemId = new URLSearchParams(window.location.search).get("id");
         
         if (currentItemId) {
-          currentItemId = parseInt(currentItemId, 10);
+            currentItemId = parseInt(currentItemId, 10);
         }
 
         this.state = {
-          item: galleryImages.find(item => item.id === currentItemId)
+            item: galleryImages.find(item => item.id === currentItemId)
         };
 
-      }
+        this.handlePrev = this.handlePrev.bind(this);
+        this.handleNext = this.handleNext.bind(this);
+    }
+
+    handlePrev = () => {
+        const prevItemID = this.state.item.id - 1;
+        this.setState({ item: galleryImages.find(item => item.id === prevItemID) });
+    };
+
+    handleNext = () => {
+        const nextItemID = this.state.item.id + 1;
+        this.setState({ item: galleryImages.find(item => item.id === nextItemID) });
+    };
 
     render() {
         const item = this.state.item;
+
+        // prev and next item IDs
+        const prevItemID = item.id - 1;
+        const nextItemID = item.id + 1;
+        
         return (
-          <div className="container">
-            <div className="item-details" key={item.id}>
-                <ItemImages />
-                <div className="item-details-info">
-                    <h1>{item.title}</h1>
-                    <p>{item.date}</p>
-                    <p>{item.media}</p>
-                    <p>{item.dimensions}</p>
+            <div className="container">
+                <div className="item-details" key={item.id}>
+                    <ItemImages />
+                    <div className="item-details-info">
+                        <h1>{item.title}</h1>
+                        <p>{item.date}</p>
+                        <p>{item.media}</p>
+                        <p>{item.dimensions}</p>
+                        <div className="prev-next-buttons">
+                            {/* previous item button */}
+                            <Link to = {{ 
+                                pathname:`/art`,
+                                search: `?id=${prevItemID}`
+                                }} 
+                                onClick={this.handlePrev} >
+                                <FontAwesomeIcon icon={faChevronLeft} />
+                            </Link>
+
+                            {/* next item button */}
+                            <Link to = {{ 
+                                pathname:`/art`,
+                                search: `?id=${nextItemID}`
+                                }} 
+                                onClick={this.handleNext} >
+                                <FontAwesomeIcon icon={faChevronRight} />
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
-          </div>
         );
     }
 }
