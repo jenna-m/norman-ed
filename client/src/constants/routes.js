b86863830592ca8646be87b7ff3eb470d9fffe25
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-09-30 16:17:03 -0400

    Update routes.js
    Update routing

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -8,7 +8,7 @@ import ArtistStatement from '../components/pages/about/artistStatement';
 import Gallery from '../components/pages/gallery/gallery';
 import PreCollegiate from '../components/pages/gallery/preCollegiate';
 import Undergrad from '../components/pages/gallery/undergrad';
-import PostToPresent from '../components/pages/gallery/postToPresent';
+import PostCollegiate from '../components/pages/gallery/postCollegiate';
 import Item from '../components/pages/gallery/item';
 import Exhibitions from '../components/pages/exhibitions';
 import Press from '../components/pages/press';
@@ -18,17 +18,17 @@ import NotFound from '../components/pages/404';
 export const Routes = 
     <div>
         <Switch>
-            <Route exact path="/" component={ Home }/>
-            <Route path="/gallery" component={ Gallery }/>
-            <Route path="/pre-collegiate" component={ PreCollegiate }/>
-            <Route path="/undergrad" component={ Undergrad }/>
-            <Route path="/post-present" component={ PostToPresent }/>
-            <Route path="/art" component={ Item }/>
-            <Route path="/exhibitions" component={ Exhibitions }/>
-            <Route path="/press" component={ Press }/>
-            <Route path="/bio" component={ Bio }/>
-            <Route path="/artist-statement" component={ ArtistStatement }/>
-            <Route path="/contact" component={ Contact }/>
-            <Route component={ NotFound }/>
+            <Route exact path="/" component={ Home } />
+            <Route path="/gallery" component={ Gallery } />
+            <Route path="/pre-collegiate" component={ PreCollegiate } />
+            <Route path="/undergrad" component={ Undergrad } />
+            <Route path="/post-collegiate" component={ PostCollegiate } />
+            <Route path="/art" component={ Item } />
+            <Route path="/exhibitions" component={ Exhibitions } />
+            <Route path="/press" component={ Press } />
+            <Route path="/bio" component={ Bio } />
+            <Route path="/artist-statement" component={ ArtistStatement } />
+            <Route path="/contact" component={ Contact } />
+            <Route component={ NotFound } />
         </Switch>
     </div>
