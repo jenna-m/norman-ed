8c13bbdf5aa08bed716ed17a5101f61b9f8d9d7f
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-01 13:42:20 -0400

    Move home component

---
+++ client/src/components/pages/home/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeGallery from './homeGallery';
+import RandomImage from '../../randomImage/randomImage';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <HomeGallery />
+            </div>
+        );
+    }
+}
+
+export default Home;
