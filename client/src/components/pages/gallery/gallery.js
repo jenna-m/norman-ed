ff5b51a980bee004bba30736377f0d92009eb371
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-01 22:40:03 -0400

    Update gallery.js
    Redesign Gallery landing page

--- client/src/components/pages/gallery/gallery.js
+++ client/src/components/pages/gallery/gallery.js
@@ -1,4 +1,3 @@
-// Renders all works, ordered by date
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { galleryImages } from '../../../constants/gallery/galleryDatabase';
@@ -6,33 +5,71 @@ import { galleryImages } from '../../../constants/gallery/galleryDatabase';
 
 class Gallery extends React.Component {
     render() {
-        let allWork = galleryImages.map(item => {
-            return (
-                <div className="item-card" key={item.id}>
-                    <div className="item-image">
-                        <Link to = {{ 
-                            pathname:`/art`,
-                            search: `?id=${item.id}`
-                        }}
-                        >
-                            <img src={item.src[0]} alt={item.title} />
-                        </Link>
+        // const for a random number 1-12
+        const preCollegiateRandomNum = Math.floor(Math.random() * 12) + 1
+        // const for a random number 13-20
+        const undergradRandomNum = Math.floor(Math.random() * 8) + 13
+        // const for a random number 21-73
+        const postCollegiateRandomNum = Math.floor(Math.random() * 53) + 21
+
+        const preCollegiateItem = galleryImages.map(item => {
+            if (item.id === preCollegiateRandomNum) {
+                return (
+                    <div className="gallery-home-item-card" key={item.id}>
+                        <div className="item-image">
+                            <Link to={{ pathname:`/${item.category}` }}>
+                                <img src={item.src[0]} alt={item.title} />
+                            </Link>
+                        </div>
                     </div>
-                </div>
-            )
+                )
+            }
         })
+
+        const undergradItem = galleryImages.map(item => {
+            if (item.id === undergradRandomNum) {
+                return (
+                    <div className="gallery-home-item-card" key={item.id}>
+                        <div className="item-image">
+                            <Link to={{ pathname:`/${item.category}` }}>
+                                <img src={item.src[0]} alt={item.title} />
+                            </Link>
+                        </div>
+                    </div>
+                )
+            }
+        })
+
+        const postCollegiateItem = galleryImages.map(item => {
+            if (item.id === postCollegiateRandomNum) {
+                return (
+                    <div className="gallery-home-item-card" key={item.id}>
+                        <div className="item-image">
+                            <Link to={{ pathname:`/${item.category}` }}>
+                                <img src={item.src[0]} alt={item.title} />
+                            </Link>
+                        </div>
+                    </div>
+                )
+            }
+        })
+        
         return (
             <div className="container">
                 <h1>Gallery</h1>
-                <div className="gallery-menu">
-                    <ul>
-                        <li>Pre-collegiate</li>
-                        <li>Undergrad</li>
-                        <li>Post-collegiate</li>
-                    </ul>
-                </div>
-                <div className="gallery-container">
-                    { allWork }
+                <div className="gallery-landing-container">
+                    <div className="category-container">
+                        <h2>Pre-collegiate</h2>
+                        { preCollegiateItem }
+                    </div>
+                    <div className="category-container">
+                        <h2>Undergrad</h2>
+                        { undergradItem }
+                    </div>
+                    <div className="category-container">
+                        <h2>Post-Collegiate</h2>
+                        { postCollegiateItem }
+                    </div>
                 </div>
             </div>
         );
