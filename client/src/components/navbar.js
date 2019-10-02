bc1d9418e196506f0cea6aa37345a11474596594
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-01 22:38:02 -0400

    Update navbar.js
    Simplify navigation, consolidate pages

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -9,23 +9,10 @@ class Navbar extends React.Component {
                     <NavLink to="/">Norman Ed</NavLink>
                 </div>
                 <ul className="menu">
-                    <li>
-                        <li className="menu-category"><NavLink to="/gallery">Gallery</NavLink></li>
-                        <ul className="dropdown">
-                            <li className="sublink"><NavLink to="/pre-collegiate">Pre-collegiate</NavLink></li>
-                            <li className="sublink"><NavLink to="/undergrad">Undergrad</NavLink></li>
-                            <li className="sublink"><NavLink to="/post-collegiate">Post-collegiate</NavLink></li>    
-                        </ul>
-                    </li>
+                    <li className="menu-category"><NavLink to="/gallery">Gallery</NavLink></li>
                     <li className="menu-category"><NavLink to="/exhibitions">Exhibitions</NavLink></li>
                     <li className="menu-category"><NavLink to="/press">Press</NavLink></li>
-                    <li>
-                        <li className="menu-category"><NavLink to="/bio">About</NavLink></li>
-                        <ul className="dropdown">
-                            <li className="sublink"><NavLink to="/bio">Biography</NavLink></li>
-                            <li className="sublink"><NavLink to="/artist-statement">Artist Statement</NavLink></li>    
-                        </ul>
-                    </li>
+                    <li className="menu-category"><NavLink to="/about">About</NavLink></li>
                     <li className="menu-category"><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
