e264aba1059edbb933a0b7eed5b611e0b0441e38
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-06 11:43:06 -0400

    Update logo

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../constants/logo.png';
 
 class Navbar extends React.Component {
     render() {
         return (
             <div className="navbar" role="navigation" aria-label="Main">
                 <div className="logo">
-                    <NavLink to="/">Norman Ed</NavLink>
+                    <NavLink to="/">
+                        <img src={logo} alt="Norman Ed Logo" />
+                    </NavLink>
                 </div>
                 <ul className="menu">
                     <li className="menu-category"><NavLink to="/gallery">Gallery</NavLink></li>
