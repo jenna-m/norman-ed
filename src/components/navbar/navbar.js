40b49e7df589dadaed0f262ebc1e3683757fe328
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-14 15:58:32 -0400

    Move navbar.js to /navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../constants/logo.png';
+import logo from '../../constants/logo.png';
 
 class Navbar extends React.Component {
     render() {
