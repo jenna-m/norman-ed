4f7bf9c81d2196bb0912cae5346f6dd290e05f06
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-14 17:12:04 -0400

    Update navigation.js
    Use react-responsive to add a media query for screen size, and render either sidebar or navbar based on screen size

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Sidebar from './sidebar';
 import Navbar from './navbar';
+import MediaQuery from 'react-responsive'
 
 class Navigation extends React.Component {
     render() {
         return (
             <div>
                 {/* Render dropdown when screen size is 768px wide*/}
-                <Sidebar />
+                <MediaQuery maxDeviceWidth={767}>
+                    <Sidebar />
+                </MediaQuery>
 
                 {/* Render Navbar when screen size is greater than 768px wide */}
-                <Navbar />
+                <MediaQuery minDeviceWidth={768}>
+                    <Navbar />
+                </MediaQuery>
             </div>
         )
     }
