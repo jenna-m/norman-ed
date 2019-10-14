4d75e972eae48b4f476cdb3e69d06ba42ef81b13
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-14 15:58:47 -0400

    Create dropdown.js

---
+++ src/components/navbar/dropdown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+
+class Dropdown extends React.Component {
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default Dropdown;
