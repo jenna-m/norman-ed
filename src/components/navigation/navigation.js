821183c3fcf5e81f086954399d498dd8b178d041
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-15 16:12:33 -0400

    Update responsive CSS

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -7,13 +7,13 @@ class Navigation extends React.Component {
     render() {
         return (
             <div>
-                {/* Render dropdown when screen size is 768px wide*/}
-                <MediaQuery maxDeviceWidth={767}>
+                {/* Render dropdown when screen size is max 899px wide */}
+                <MediaQuery maxDeviceWidth={899}>
                     <Sidebar />
                 </MediaQuery>
 
-                {/* Render Navbar when screen size is greater than 768px wide */}
-                <MediaQuery minDeviceWidth={768}>
+                {/* Render Navbar when screen size is at least 900px wide */}
+                <MediaQuery minDeviceWidth={900}>
                     <Navbar />
                 </MediaQuery>
             </div>
