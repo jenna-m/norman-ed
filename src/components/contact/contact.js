22417aacf88503e7284586a0a71a38322fb9bb41
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-15 16:17:44 -0400

    Update contact.js and contactForm.js

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,50 +1,7 @@
 import React from 'react';
-import axios from 'axios';
 import ContactForm from './contactForm';
 
 class Contact extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            name: '',
-            email: '',
-            message: '',
-            buttonText: 'Submit'
-        };
-    }
-
-    formSubmit = (e) => {
-        e.preventDefault();
-
-        this.setState({
-            buttonText: 'Submitting...'
-        });
-
-        let data = {
-            name: this.state.name,
-            email: this.state.email,
-            message: this.state.message
-        }
-
-        axios.post('API_URI', data)
-        .then( res => {
-            this.setState({ sent: true }, this.resetForm())
-        })
-        .catch( () => {
-            console.log('Message not sent.')
-        })
-    }
-
-    resetForm = () => {
-        this.setState({
-            name: '',
-            email: '',
-            message: '',
-            buttonText: 'Message Sent'
-        });
-    }
-
     render() {
         return (
             <div className="container">
--- src/components/contact/contactForm.js
+++ src/components/contact/contactForm.js
@@ -1,6 +1,49 @@
 import React from 'react';
+import axios from 'axios';
 
 class ContactForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            email: '',
+            message: '',
+            buttonText: 'Submit'
+        };
+    }
+
+    formSubmit = (e) => {
+        e.preventDefault();
+
+        this.setState({
+            buttonText: 'Submitting...'
+        });
+
+        let data = {
+            name: this.state.name,
+            email: this.state.email,
+            message: this.state.message
+        }
+
+        axios.post('API_URI', data)
+        .then( res => {
+            this.setState({ sent: true }, this.resetForm())
+        })
+        .catch( () => {
+            console.log('Message not sent.')
+        })
+    }
+
+    resetForm = () => {
+        this.setState({
+            name: '',
+            email: '',
+            message: '',
+            buttonText: 'Message Sent'
+        });
+    }
+    
     render() {
         return (
             <div className="contact-form-container">
