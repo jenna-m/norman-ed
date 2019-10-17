c367537e525e239e8a549221093780b0f3488248
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-16 22:30:41 -0400

    Update config.js
    Hard coded EmailJS values

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
+// I had to hard code these, but I got it to work now! ¯\_(ツ)_/¯
+
 export const env = {
-  REACT_APP_EMAILJS_RECEIVER: process.env.REACT_APP_EMAILJS_RECEIVER,
-  REACT_APP_EMAILJS_TEMPLATEID: process.env.REACT_APP_EMAILJS_TEMPLATEID,
-  REACT_APP_EMAILJS_USERID: process.env.REACT_APP_EMAILJS_USERID
+  REACT_APP_EMAILJS_RECEIVER: 'thesemdrnsocks@gmail.com',
+  REACT_APP_EMAILJS_TEMPLATEID: 'normanedcontactform',
+  REACT_APP_EMAILJS_USERID: 'user_cVacpT4HoGWc7Omos0X8P'
 };
