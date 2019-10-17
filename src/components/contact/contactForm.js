f6df3ff92d40489315c941ff6cdbe862ae316e48
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-17 00:50:11 -0400

    Update contactForm.js
    Update Yup schema and add logic for random name placeholders

--- src/components/contact/contactForm.js
+++ src/components/contact/contactForm.js
@@ -15,19 +15,31 @@ let template_params = {
     'message': 'message_value'
 }
  
+
 // EmailJS consts
 const service_id = 'default_service';
 const template_id = 'normanedcontactform';
 const emailJSUserID = 'user_cVacpT4HoGWc7Omos0X8P'
 
+
+// Random name placeholder -- ADD MORE NAMES!
+const nameArray = [
+    'The Dude'
+];
+
+let randomNameIndex = Math.floor(Math.random() * nameArray.length);
+
+const randomName = nameArray[randomNameIndex];
+
+
 // Yup schema
 const schema = yup.object().shape({
     name: yup.string()
         .min(2, 'Name must be at least 2 characters')
-        .max(70, 'Name cannot be over 70 characters')
+        .max(50, 'Name cannot be over 50 characters')
         .required('Name is required'),
     email: yup.string()
-        .email('Invalid email')
+        .email('Email is invalid')
         .required('Email is required'),
     message: yup.string()
         .min(10, 'Message must be at least 10 characters')
@@ -35,11 +47,14 @@ const schema = yup.object().shape({
         .required('Message is required')
 });
 
+
+// Dynamic button text
 export const legendMessage = {
-    default: 'Contact',
+    default: 'Send message',
     messageSent: 'Message sent!',
-    messageSentError: 'Error sending message'
+    messageSentError: 'Error; try again'
 }
+
 
 // Submit method
 const handleSubmit = async (values, actions, setLegend) => {
@@ -87,45 +102,47 @@ export const ContactForm = () => {
                     {({ errors }) => (
                     <Form id="contact-form" role="form">
                         <fieldset>
-                            <h1>{ legend }</h1>
                             { /* Name Input */ }
                             <label for name="name">Name</label>
                             <Field
+                                className="field"
                                 type="name"
                                 name="name"
-                                placeholder="Your name"
+                                placeholder={randomName}
                                 onBlur={handleBlur}
                             />
                             <ErrorMessage
                                 name="name"
-                                component="span"
+                                component="h6"
                             />
 
                             { /* Email Input */ }
                             <label for name="email">Email</label>
                             <Field
+                                className="field"
                                 type="email"
                                 name="email"
-                                placeholder="Your email"
+                                placeholder="you@example.com"
                                 onBlue={handleBlur}
                             />
                             <ErrorMessage
                                 name="email"
-                                component="span"
+                                component="h6"
                             />
 
                             { /* Message Input */ }
                             <label for name="message">Message</label>
                             <Field
+                                className="textarea"
                                 type="message"
                                 name="message"
-                                placeholder="Your message"
+                                placeholder="Dear Norman..."
                                 component="textarea"
                                 onBlue={handleBlur}
                             />
                             <ErrorMessage
                                 name="message"
-                                component="span"
+                                component="h6"
                             />
 
                             <button
@@ -133,7 +150,7 @@ export const ContactForm = () => {
                                 name="submit"
                                 type="submit"
                             >
-                                Send
+                                { legend }
                             </button>
                         </fieldset>
                     </Form>
