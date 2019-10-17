0b0dd6b683a3ee441837614b5a005fcfdfc7b48f
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-17 00:50:18 -0400

    Update contact.js

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -6,6 +6,7 @@ class Contact extends React.Component {
     render() {
         return (
             <div className="container">
+                <h1>Contact</h1>
                 <ContactForm env={env} />
             </div>
         );
