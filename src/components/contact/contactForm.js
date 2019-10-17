caef89cc6b9c9f379c28a2d1dbad8693ae7b32a4
Jenna <19200395+thesemdrnsocks@users.noreply.github.com>
2019-10-16 22:30:23 -0400

    Update contactForm.js
    Implement formik form, yup validation, and EmailJS

--- src/components/contact/contactForm.js
+++ src/components/contact/contactForm.js
@@ -1,95 +1,146 @@
-// Not yet fully functioning; still need to add name and email fields
+import React, { useState } from 'react';
+import * as emailjs from 'emailjs-com';
+import {
+    Formik,
+    Form,
+    Field,
+    ErrorMessage
+} from 'formik';
+import * as yup from 'yup';
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+// EmailJS template params
+let template_params = {
+    'senderEmail': 'senderEmail_value',
+    'senderName': 'senderName_value',
+    'message': 'message_value'
+}
+ 
+// EmailJS consts
+const service_id = 'default_service';
+const template_id = 'normanedcontactform';
+const emailJSUserID = 'user_cVacpT4HoGWc7Omos0X8P'
 
-export default class ContactForm extends Component {
-    state = {
-        message: '',
-        formSubmitted: false
-    };
+// Yup schema
+const schema = yup.object().shape({
+    name: yup.string()
+        .min(2, 'Name must be at least 2 characters')
+        .max(70, 'Name cannot be over 70 characters')
+        .required('Name is required'),
+    email: yup.string()
+        .email('Invalid email')
+        .required('Email is required'),
+    message: yup.string()
+        .min(10, 'Message must be at least 10 characters')
+        .max(600, 'Message cannot be over 600 characters')
+        .required('Message is required')
+});
 
-    handleCancel = this.handleCancel.bind(this);
-    handleChange = this.handleChange.bind(this);
-    handleSubmit = this.handleSubmit.bind(this);
+export const legendMessage = {
+    default: 'Contact',
+    messageSent: 'Message sent!',
+    messageSentError: 'Error sending message'
+}
 
-    static sender = 'sender@example.com';
+// Submit method
+const handleSubmit = async (values, actions, setLegend) => {
+    const { name, email, message } = values;
+    const { setStatus, setSubmitting, setErrors, resetForm } = actions;
 
-    handleCancel() {
-        this.setState({
-            message: ''
+    template_params['senderName'] = name;
+    template_params['senderEmail'] = email;
+    template_params['message'] = message;
+
+    try {
+        await emailjs.send(service_id, template_id, template_params, emailJSUserID);
+
+        resetForm();
+        setLegend(legendMessage.messageSent)
+    }
+    catch(error) {
+        setSubmitting(false);
+        setErrors(JSON.stringify(error));
+        setStatus({
+            msg: legendMessage.messageSentError
         });
-      }
-    
-    handleChange(event) {
-    this.setState({
-        message: event.target.value
-    });
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-
-        const {
-            REACT_APP_EMAILJS_RECEIVER: receiverEmail,
-            REACT_APP_EMAILJS_TEMPLATEID: template,
-            REACT_APP_EMAILJS_USERID: user,
-        } = this.props.env;
-
-        this.sendMessage(
-            template,
-            this.sender,
-            receiverEmail,
-            this.state.message,
-            user
-        );
-
-        this.setState({
-            formSubmitted: true
-        });
-    }
-
-    sendMessage(templateId, senderEmail, receiverEmail, message, user) {
-        window.emailjs.send('default_service', templateId, {
-            senderEmail, receiverEmail, message
-        },
-        user
-        )
-        .then(res => {
-            this.setState({
-                formEmailSent: true
-            });
-        })
-        // Error handling
-        .catch(err => console.error('Failed to send message. Error: ', err));
-    }
-
-    render() {
-        return (
-            <div>
-            { console.log(process.env) }
-            <form className="contact-form" onSubmit={this.handleSubmit}>
-                <textarea 
-                    className="text-input"
-                    id="message-entry"
-                    name="message-entry"
-                    onChange={this.handleChange}
-                    placeholder="Enter your message here"
-                    required
-                    value={this.state.message}
-                />
-                <div className="button-group">
-                    <button className="button-cancel" onClick={this.handleCancel}>
-                        Cancel
-                    </button>
-                    <input type="submit" value="Submit" className="button-submit" />
-                </div>
-            </form>
-            </div>
-        );
+        setLegend(legendMessage.messageSentError)
     }
 }
 
-ContactForm.propTypes = {
-    env: PropTypes.object.isRequired
-};
+export const ContactForm = () => {
+    const [legend, setLegend] = useState(legendMessage.default);
+
+    const handleBlur = () => {
+        setLegend(legendMessage.default)
+    }
+
+    return (
+        <div>
+            <Formik
+                initialValues = {{
+                    name: '',
+                    email: '',
+                    message: ''
+                }}
+                validationSchema = {schema}
+                onSubmit = {(values, actions) => handleSubmit(values, actions, setLegend)}
+                >
+                    {({ errors }) => (
+                    <Form id="contact-form" role="form">
+                        <fieldset>
+                            <h1>{ legend }</h1>
+                            { /* Name Input */ }
+                            <label for name="name">Name</label>
+                            <Field
+                                type="name"
+                                name="name"
+                                placeholder="Your name"
+                                onBlur={handleBlur}
+                            />
+                            <ErrorMessage
+                                name="name"
+                                component="span"
+                            />
+
+                            { /* Email Input */ }
+                            <label for name="email">Email</label>
+                            <Field
+                                type="email"
+                                name="email"
+                                placeholder="Your email"
+                                onBlue={handleBlur}
+                            />
+                            <ErrorMessage
+                                name="email"
+                                component="span"
+                            />
+
+                            { /* Message Input */ }
+                            <label for name="message">Message</label>
+                            <Field
+                                type="message"
+                                name="message"
+                                placeholder="Your message"
+                                component="textarea"
+                                onBlue={handleBlur}
+                            />
+                            <ErrorMessage
+                                name="message"
+                                component="span"
+                            />
+
+                            <button
+                                id="submit"
+                                name="submit"
+                                type="submit"
+                            >
+                                Send
+                            </button>
+                        </fieldset>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    )
+}
+
+export default ContactForm;
