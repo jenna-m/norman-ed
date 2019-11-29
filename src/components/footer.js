cc515b0c6e72bc14ba6ef2c67ca35f73c155970e
Jenna M <19200395+jenna-m@users.noreply.github.com>
2019-11-28 21:08:40 -0500

    Update footer.js
    Update credit link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
 class Footer extends React.PureComponent {
-    render() {
-        return (
-            <div className="footer">
-                <p>Artwork © Norman Ed 1975-2019</p>
-                <p>Website development by <a href="https://github.com/jenna-m">jenna-m</a></p>
-            </div>
-
-        );
-    }
+	render() {
+		return (
+			<div className="footer">
+				<p>Artwork © Norman Ed 1975-2019</p>
+				<p>
+					Website development by{' '}
+					<a
+						href="https://jennamichaels.dev"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Jenna Michaels
+					</a>
+				</p>
+			</div>
+		);
+	}
 }
 
 export default Footer;
